805f9df0cddafe745762f3eb63db45836c127f41
rappanuiok <fran_rappa@outlook.com>
2021-12-08 18:09:03 -0300

    probando firebase

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+const productos = [];
 const db = firebase.firestore();
 
-console.log(db);
+db.collection("productos")
+  .get()
+  .then((res) => productos.push(...res.docs.map((doc) => doc.data())))
+  .then(() => console.log(productos));
 
 // --------------------------------------------------
 $(".li__indumentaria").click(() => $(".ul__hide").toggle("fast"));
