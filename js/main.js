414d24aee88e20b3f5110af0f455fa1b6ee34253
rappanuiok <fran_rappa@outlook.com>
2021-12-09 18:06:59 -0300

    firebase deleted

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-const productos = [];
-const db = firebase.firestore();
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyAHJtu7kxN_bRpDQMqvB--AMcQNtYd3-kE",
+  authDomain: "prueba-104e5.firebaseapp.com",
+  projectId: "prueba-104e5",
+  storageBucket: "prueba-104e5.appspot.com",
+  messagingSenderId: "799653477259",
+  appId: "1:799653477259:web:339c78ddf189271c97cd8e",
+});
 
-db.collection("productos")
-  .get()
-  .then((res) => productos.push(...res.docs.map((doc) => doc.data())))
-  .then(() => console.log(productos));
+const db = firebaseApp.firestore();
+
+console.log(db);
+// db.collection("productos")
+//   .get()
+//   .then((res) => productos.push(...res.docs.map((doc) => doc.data())))
+//   .then(() => console.log(productos));
 
 // --------------------------------------------------
 $(".li__indumentaria").click(() => $(".ul__hide").toggle("fast"));
