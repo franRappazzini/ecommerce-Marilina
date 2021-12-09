953ed75c20800a1c41726f84900842334d5a0ec1
rappanuiok <fran_rappa@outlook.com>
2021-12-09 19:18:40 -0300

    new commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+const productos = [];
 const db = firebase.firestore();
 
 console.log(db);
 
-// db.collection("productos")
-//   .get()
-//   .then((res) => productos.push(...res.docs.map((doc) => doc.data())))
-//   .then(() => console.log(productos));
+db.collection("productos")
+  .get()
+  .then((res) => productos.push(...res.docs.map((doc) => doc.data())))
+  .then(() => console.log(productos));
 
 // --------------------------------------------------
 $(".li__indumentaria").click(() => $(".ul__hide").toggle("fast"));
