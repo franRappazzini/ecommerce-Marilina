919459017ae8d5c6fbc7eab18c70b1f22fe074ab
rappanuiok <fran_rappa@outlook.com>
2021-12-09 19:16:24 -0300

    new commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAHJtu7kxN_bRpDQMqvB--AMcQNtYd3-kE",
-  authDomain: "prueba-104e5.firebaseapp.com",
-  projectId: "prueba-104e5",
-  storageBucket: "prueba-104e5.appspot.com",
-  messagingSenderId: "799653477259",
-  appId: "1:799653477259:web:339c78ddf189271c97cd8e",
-});
-
-const db = firebaseApp.firestore();
+const db = firebase.firestore();
 
 console.log(db);
+
 // db.collection("productos")
 //   .get()
 //   .then((res) => productos.push(...res.docs.map((doc) => doc.data())))
