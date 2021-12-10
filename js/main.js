011e61b6125eb3694cfc38eebcdbbe94cd856725
rappanuiok <fran_rappa@outlook.com>
2021-12-10 17:28:23 -0300

    cards creadas

--- js/main.js
+++ js/main.js
@@ -15,6 +15,11 @@ class Producto {
 }
 
 const productos = [];
+const categorias = [];
+
+/**
+ * FUNCTIONS
+ */
 
 // -----renderiza los productos en el DOM-----
 function renderizarProductos() {
@@ -29,7 +34,60 @@ function renderizarProductos() {
             <div class="card-body">
               <h5 class="card-title">${p.nombre}</h5>
               <p class="card-text">${p.descripcion}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${p.id}">
+                Ver más
+              </button>
+
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal${p.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">${p.nombre}</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body row">
+                      <section class="col-12 col-md-6">
+                        <div id="carousel${p.id}" class="carousel slide" data-bs-ride="carousel">
+                          <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                          </div>
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img src="../assets/img/GA025006273.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="../assets/img/GA025006273.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="../assets/img/GA025006273.jpg" class="d-block w-100" alt="...">
+                            </div>
+                          </div>
+                          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                          </button>
+                          <button class="carousel-control-next" type="button" data-bs-target="#carousel${p.id}" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                          </button>
+                        </div>
+                      </section>
+
+                      <section class="col-12 col-md-6">
+                        <p>Descripcion: ${p.descripcion}</p>
+                        <p>Precio: $${p.precio}</p>
+                      </section>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-success" data-bs-dismiss="modal">Consultar stock por WhatsApp</button>
+                    </div>
+                  </div>
+                </div>
+              </div>          
             </div>
           </div>
         </article>
@@ -37,6 +95,7 @@ function renderizarProductos() {
     });
 }
 
+// -----spinner mientras cargan los productos-----
 function loader() {
   productos.length === 0 &&
     $(".productos__container").append(`
