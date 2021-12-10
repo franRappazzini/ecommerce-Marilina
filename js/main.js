5dd38674633a7edfd5a6ec7e13c5492759d138d8
rappanuiok <fran_rappa@outlook.com>
2021-12-09 23:40:33 -0300

    new commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 const productos = [];
 
+// -----renderiza los productos en el DOM-----
 function renderizarProductos() {
   console.log(productos);
 
   productos.length > 0 &&
     productos.forEach((p) => {
       $(".productos").append(`
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="${p.foto1}">
-      <div class="card-body">
-        <h5 class="card-title">${p.nombre}</h5>
-        <p class="card-text">${p.descripcion}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  `);
+        <div class="card col-6 col-md-3" style="width: auto;">
+          <img src="..." class="card-img-top" alt="${p.foto1}">
+          <div class="card-body">
+            <h5 class="card-title">${p.nombre}</h5>
+            <p class="card-text">${p.descripcion}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      `);
     });
 }
 
