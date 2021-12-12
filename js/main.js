deb8510076224dfcf98fe44c2ca10f90cb3204ff
rappanuiok <fran_rappa@outlook.com>
2021-12-12 10:12:00 -0300

    new commit

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ const productos = [];
 /**
  * FUNCTIONS
  */
+
 // -----filtra por categoria-----
 function filterCategoria(productos, orderByCategorias, categoria) {
   productos.forEach((p) => {
@@ -50,26 +51,36 @@ function renderizarProductos(productos, categoria) {
     $(".productos").append(`
         <article class="col-6 col-md-3 mb-3">
           <div class="card" style="width: auto;">
-            <img src="../assets/img/GA025006273.jpg" class="card-img-top" alt="${p.foto1}" style="height: 12rem; object-fit: cover;">
+            <img src="../assets/img/GA025006273.jpg" class="card-img-top" alt="${
+              p.nombre
+            }" style="height: 12rem; object-fit: cover;">
             <div class="card-body">
               <h5 class="card-title">${p.nombre}</h5>
               <p class="card-text">${p.descripcion}</p>
 
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${p.id}">
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${
+                p.id
+              }">
                 Ver más
               </button>
 
               <!-- Modal -->
-              <div class="modal fade" id="exampleModal${p.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
+              <div class="modal fade" id="exampleModal${
+                p.id
+              }" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">${p.nombre}</h5>
+                      <h5 class="modal-title" id="exampleModalLabel">${
+                        p.nombre
+                      }</h5>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body row">
                       <section class="col-12 col-md-6">
-                        <div id="carousel${p.id}" class="carousel slide" data-bs-ride="carousel">
+                        <div id="carousel${
+                          p.id
+                        }" class="carousel slide" data-bs-ride="carousel">
                           <div class="carousel-indicators">
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -77,20 +88,28 @@ function renderizarProductos(productos, categoria) {
                           </div>
                           <div class="carousel-inner hover__zoom">
                             <div class="carousel-item active">
-                              <img src="${p.img1}" class="d-block w-100" alt="...">
+                              <img src="${
+                                p.img1
+                              }" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item">
-                              <img src="${p.img2}" class="d-block w-100" alt="...">
+                              <img src="${
+                                p.img2
+                              }" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item">
-                              <img src="${p.img3}" class="d-block w-100" alt="...">
+                              <img src="${
+                                p.img3
+                              }" class="d-block w-100" alt="...">
                             </div>
                           </div>
                           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                           </button>
-                          <button class="carousel-control-next" type="button" data-bs-target="#carousel${p.id}" data-bs-slide="next">
+                          <button class="carousel-control-next" type="button" data-bs-target="#carousel${
+                            p.id
+                          }" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                           </button>
@@ -99,12 +118,16 @@ function renderizarProductos(productos, categoria) {
 
                       <section class="col-12 col-md-6">
                         <p>Descripcion: ${p.descripcion}</p>
-                        <p>Precio: $${p.precio}</p>
+                        <p>Precio: $${new Intl.NumberFormat().format(
+                          p.precio
+                        )}</p>
                         <p>Categoria: ${p.categoria}</p>
                       </section>
                     </div>
                     <div class="modal-footer">
-                      <a href="https://wa.me/5493402449859/?text=Hola, cómo estás? Queria saber si te queda stock de ${p.nombre} en talle..."
+                      <a href="https://wa.me/5493402449859/?text=Hola, cómo estás? Queria saber si te queda stock de ${
+                        p.nombre
+                      } en talle..."
                       class="btn btn-success"
                       target="_blank" rel="noopener noreferrer"
                       >
